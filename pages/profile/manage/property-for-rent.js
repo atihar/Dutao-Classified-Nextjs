58a150c248bb2c80e69bc2a24bf739b9c735a4aa
Atihar Hossen Mahir <mahir_1990@yahoo.com>
2022-07-20 17:27:23 +0400

    user manage view changes

--- pages/profile/manage/property-for-rent.js
+++ pages/profile/manage/property-for-rent.js
@@ -11,6 +11,7 @@ import { Store } from '../../../lib/Store';
 import { useRouter } from 'next/router';
 import Image from "next/image"
 import useTranslation from 'next-translate/useTranslation'
+import moment from "moment"
 
 
 export default function ManagePropertyForRent() {
@@ -121,77 +122,63 @@ export default function ManagePropertyForRent() {
                 {loading && <p className="text-base">loading...</p>}
                   <hr></hr>
                 {posts && posts.map((property) => (
-                <div className="sm:flex justify-center' py-2" key={property._id} >
-                  <div className="sm:flex w-full rounded-lg bg-white shadow-lg">
-                  <Image
-                        src={`https://dutao-public.s3.amazonaws.com/${property.images[0]}`}
-                        alt="dutao image"
-                        width={380}
-                        height={240}
-                        className="object-cover rounded-t-lg"
-                    />
-                    <div className="py-4 px-6 w-full">
-                      <div className="flex items-center justify-between mb-2">
-                        <div>
-                          <h5 className="text-gray-500 text-xl font-light">
-                              <TextTruncate
-                                line={2}
-                                element="span"
-                                truncateText="…"
-                                text={property.title}
-                            /></h5>
-                        </div>
-                        <div>
-                        <Link href={'/property-for-rent/'+ property._id}>
-                        <p className="flex border ml-5 r-0 border-red-600 text-red-600 uppercase px-3 py-2 rounded-full text-[9px] tracking-wide hover:cursor-pointer">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
-                            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-                            <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-                            </svg><button className='pl-2'>View</button></p></Link>
-
-                        {/* <p className="flex border mt-2 ml-5 r-0 border-black-600 text-white bg-black uppercase px-3 py-2 rounded-full text-[9px] tracking-wide hover:cursor-pointer">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
-                            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-                            <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
-                            </svg><span className='pl-2'>Edit</span></p> */}
-                        
-                        <p className="flex border mt-2 ml-5 r-0 border-red-600 text-white bg-red-600 uppercase px-3 py-2 rounded-full text-[9px] tracking-wide hover:cursor-pointer">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash-fill" viewBox="0 0 16 16">
-                            <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
-                            </svg><button onClick={showpopupHandler} className='pl-2'>Delete</button></p>
-                            { popUp && 
-                          <div className="absolute left-[30%] top-[30%] max-w-screen-sm  p-8 bg-white rounded-lg shadow-2xl">
-                              <h2 className="text-lg font-bold">Are you sure you want to do that?</h2>
-
-                              <p className="mt-2 text-sm text-gray-500">
-                                Doing that could have cause some issues elsewhere, are you 100% sure it's OK?
-                              </p>
-
-                              <div className="flex items-center justify-end mt-8 text-xs">
-                                <button type="button" onClick={() => deleteHandler(property._id, property.images)} className="px-4 py-2 font-medium text-green-600 rounded bg-green-50">Yes, I'm sure</button>
-                                <button type="button" onClick={hidepopupHandler} className="px-4 py-2 ml-2 font-medium text-gray-600 rounded bg-gray-50">No, go back</button>
-                              </div>
-                          </div>}
-                        </div>       
-                      </div>
-                      
-
-                      <p className="mb-1 text-lg font-bold text-red-600">AED {property.price}</p>
-
-                      <p className="text-gray-400 py-1 text-xs">Last updated 3 mins ago</p>
-
-                      <p className="mb-2 text-sm pt-1 text-gray-500">{property.bedroom} Bed - {property.bathroom} Baths - {property.size}sqft</p>
-
-                        <p className='flex text-sm py-1 text-gray-500'>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="14" height="18" fill="currentColor" className="bi bi-geo-alt-fill" viewBox="0 0 16 16">
-                            <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
-                            </svg>
-                            {property.address}
-                        </p>  
-                        
-                    </div>
-                  </div>
-                </div>
+                 <div className=" flex justify-center' py-2" key={property._id} >
+                 <div className="md:flex w-full rounded-lg bg-white shadow-lg">
+                 <Image
+                       src={`https://dutao-public.s3.amazonaws.com/${property.images[0]}`}
+                       alt="dutao image"
+                       width={340}
+                       height={200}
+                       className="object-cover rounded-t-lg"
+                   />
+                   <div className="py-2 px-6 w-full">
+                     <div className="flex items-center justify-between">
+                       <div className="relative w-full">
+                         <h5 className="text-gray-500 text-xl font-light">
+                             <TextTruncate
+                               line={1}
+                               element="span"
+                               truncateText="…"
+                               text={property.title}
+                           /></h5>
+                       
+                       <div className="absolute inline-flex right-0 top-16 lg:top-0">
+                       <Link href={'/property-for-rent/'+ property._id}>
+                         <p className="border-black border-[1px] px-2 py-1 rounded-lg bg-white hover:bg-gray-100 hover:cursor-pointer">
+                           <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" className="bi bi-eye" viewBox="0 0 16 16">
+                           <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
+                           <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
+                           </svg>
+                         </p>
+                       </Link>
+                       
+                       <p className="flex border ml-2 r-0 border-red-600 text-white bg-red-600 uppercase px-3 py-1 rounded-lg text-xs tracking-wide hover:cursor-pointer">
+                       <button onClick={showpopupHandler} className='pl-2'>Remove</button></p>
+                           { popUp && 
+                         <div className="absolute left-[30%] top-[30%] max-w-screen-sm  p-8 bg-white rounded-lg shadow-2xl">
+                             <h2 className="text-lg font-bold">Are you sure you want to do that?</h2>
+ 
+                             <p className="mt-2 text-sm text-gray-500">
+                               Doing that could have cause some issues elsewhere, are you 100% sure it's OK?
+                             </p>
+ 
+                             <div className="flex items-center justify-end mt-8 text-xs">
+                               <button type="button" onClick={() => deleteHandler(property._id, property.images)} className="px-4 py-2 font-medium text-green-600 rounded bg-green-50">Yes, I'm sure</button>
+                               <button type="button" onClick={hidepopupHandler} className="px-4 py-2 ml-2 font-medium text-gray-600 rounded bg-gray-50">No, go back</button>
+                             </div>
+                         </div>}
+                       </div> 
+                       </div>   
+                     </div>
+                             
+                     <p className="text-gray-700 pb-1 text-xs">{property.category}</p>
+                     <p className="text-sm font-bold text-red-600">AED {property.price}</p>
+ 
+                     <p className="text-gray-400 pt-1 text-xs">Last updated {moment(property.createdAt).startOf('hour').fromNow()}</p>
+                       <p className="mb-2 text-sm pt-1 text-gray-500">{property.bedroom} Bed - {property.bathroom} Baths - {property.size}sqft</p>     
+                   </div>
+                 </div>
+               </div>
                 ))}
                 </div>
                 
